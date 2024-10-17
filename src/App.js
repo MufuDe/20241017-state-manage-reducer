d4e860b59a1d8e49573897011f22af4f161b7b51
chendechuan <chen0802@qq.com>
2024-10-17 14:50:13 +0800

    将 useState 迁移到 useReducer： 1. 将设置状态的逻辑 修改 成 dispatch 的一个 action； 2. 编写 一个 reducer 函数； 3. 在组件中 使用 reducer。

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import { useState } from 'react';
-import AddTask from './AddTask.js';
-import TaskList from './TaskList.js';
+import { useReducer } from "react";
+import AddTask from "./AddTask.js";
+import TaskList from "./TaskList.js";
 
 export default function TaskApp() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
   function handleAddTask(text) {
-    setTasks([
-      ...tasks,
-      {
-        id: nextId++,
-        text: text,
-        done: false,
-      },
-    ]);
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
   }
 
   function handleChangeTask(task) {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
+    dispatch({
+      type: "changed",
+      task: task,
+    });
   }
 
   function handleDeleteTask(taskId) {
-    setTasks(tasks.filter((t) => t.id !== taskId));
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
   }
 
   return (
@@ -45,9 +40,39 @@ export default function TaskApp() {
   );
 }
 
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("未知 action: " + action.type);
+    }
+  }
+}
+
 let nextId = 3;
 const initialTasks = [
-  {id: 0, text: '参观卡夫卡博物馆', done: true},
-  {id: 1, text: '看木偶戏', done: false},
-  {id: 2, text: '打卡列侬墙', done: false},
+  { id: 0, text: "参观卡夫卡博物馆", done: true },
+  { id: 1, text: "看木偶戏", done: false },
+  { id: 2, text: "打卡列侬墙", done: false },
 ];
