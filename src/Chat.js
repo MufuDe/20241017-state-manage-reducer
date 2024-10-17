7445cb15e411ec97c92f0dab91276ac51f186da7
chendechuan <chen0802@qq.com>
2024-10-17 15:21:07 +0800

    1. 通过事件处理函数 dispatch actions;

---
+++ src/Chat.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function Chat({ contact, message, dispatch }) {
+  return (
+    <section className="chat">
+      <textarea
+        value={message}
+        placeholder={"和 " + contact.name + " 聊天"}
+        onChange={(e) => {
+          // TODO: 派发 edited_message
+          // (从 e.target.value 获取输入框的值)
+        }}
+      />
+      <br />
+      <button>发送到 {contact.email}</button>
+    </section>
+  );
+}
