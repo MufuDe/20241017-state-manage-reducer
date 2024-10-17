3a32a4e69b30c8379b11dcbe70728d1d7ce9cb92
chendechuan <chen0802@qq.com>
2024-10-17 15:27:32 +0800

    1. 完善dispatch 相应的 action；

--- src/Chat.js
+++ src/Chat.js
@@ -7,8 +7,10 @@ export default function Chat({ contact, message, dispatch }) {
         value={message}
         placeholder={"和 " + contact.name + " 聊天"}
         onChange={(e) => {
-          // TODO: 派发 edited_message
-          // (从 e.target.value 获取输入框的值)
+          dispatch({
+            type: "edited_message",
+            message: e.target.value,
+          });
         }}
       />
       <br />
