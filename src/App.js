d863ee91ca652a70a578ae7fb116d405afc0fc0c
chendechuan <chen0802@qq.com>
2024-10-17 16:51:20 +0800

    1. 提供 context；

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import Section from "./Section.js";
 
 export default function Page() {
   return (
-    <Section>
-      <Heading level={1}>主标题</Heading>
-      <Section>
-        <Heading level={2}>副标题</Heading>
-        <Heading level={2}>副标题</Heading>
-        <Heading level={2}>副标题</Heading>
-        <Section>
-          <Heading level={3}>子标题</Heading>
-          <Heading level={3}>子标题</Heading>
-          <Heading level={3}>子标题</Heading>
-          <Section>
-            <Heading level={4}>子子标题</Heading>
-            <Heading level={4}>子子标题</Heading>
-            <Heading level={4}>子子标题</Heading>
+    <Section level={1}>
+      <Heading>主标题</Heading>
+      <Section level={2}>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Heading>副标题</Heading>
+        <Section level={3}>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Heading>子标题</Heading>
+          <Section level={4}>
+            <Heading>子子标题</Heading>
+            <Heading>子子标题</Heading>
+            <Heading>子子标题</Heading>
           </Section>
         </Section>
       </Section>
